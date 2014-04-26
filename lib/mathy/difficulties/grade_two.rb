ab4761e6fd8f5b715e8e147a68b85416da07937a
mo khan <mo@mokhan.ca>
2014-04-26 12:56:39 -0600

    let the difficulty decide what operations the user can choose.

--- lib/mathy/difficulties/grade_two.rb
+++ lib/mathy/difficulties/grade_two.rb
@@ -4,6 +4,14 @@ module Mathy
       def next_operands
         [rand(20), rand(20)].sort.reverse
       end
+
+      def choose_operation(console)
+        verifier = Verification.new
+        console.operation?([
+          Operations::Addition.new(verifier),
+          Operations::Subtraction.new(verifier)
+        ])
+      end
     end
   end
 end
